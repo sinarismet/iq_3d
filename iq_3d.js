ffed1ab4fc913c5966c024ea9e28b98ed684c619
ismet sinar <info@nfo.com.tr>
2022-03-07 00:30:26 +0300

    new

---
+++ iq_3d.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+const firebase = require('firebase-admin');
+const serviceAccount = require('./evakey.json');
+var server = require('http').Server(app);
+const fs = require('fs');
+var decode = require('decode-html');
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://evapay-91708-default-rtdb.europe-west1.firebasedatabase.app"
+});
+var db = firebase.database();
+
+const port = 3002;
+server.listen(port);
+console.log("started.");
+
+app.get('/iq/securepage', function (request, res, next) {
+    var threed_data = "";
+    var ref = db.ref("iq/" + request.query.b);
+    ref.once("value", function (snapshot) {
+        var values = snapshot.val();
+        let change_to_form = Buffer.from(values.message, 'base64');
+        res.send(decode(change_to_form.toString('utf-8')));
+    });
+});
